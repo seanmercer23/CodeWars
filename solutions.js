79cdc3991cdc1b36a0176ef7926dfdbd2c2031e4
Sean Mercer <seansmercer@gmail.com>
2019-05-28 15:17:48 -0400

    Two solutions added

--- solutions.js
+++ solutions.js
@@ -11,3 +11,23 @@ function hero (bullets, dragons) {
 function reverseWords (str) {
     return str.split("").reverse().join("").split(" ").reverse().join(" ");
 }
+
+// Complementary DNA
+const compliments = {
+    "A" : "T",
+    "T" : "A",
+    "C" : "G",
+    "G" : "C"
+    }
+  function DNAStrand(dna){
+    return dna.split('').map(function(pair) {return compliments[pair]}).join('')
+  }
+
+// Is n divisible by x and y?
+function isDivisible(n, x, y) {
+    if (n % x === 0 && n % y === 0) {
+      return true
+    } else {
+      return false
+      }
+  }
