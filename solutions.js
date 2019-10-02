7c19c4bb2ec6f6dbb80fd3367ae6cf03ec35012a
Sean Mercer <seansmercer@gmail.com>
2019-10-02 16:39:49 -0400

    Switch it up

--- solutions.js
+++ solutions.js
@@ -216,3 +216,29 @@ function feast(beast, dish) {
   return beast[0] === dish[0] && 
   beast[beast.length - 1] === dish[dish.length - 1]
 }
+
+// Switch it up
+function switchItUp(number){
+  switch (true) {
+    case number === 0: return "Zero"
+    break
+    case number === 1: return "One"
+    break
+    case number === 2: return "Two"
+    break
+    case number === 3: return "Three"
+    break
+    case number === 4: return "Four"
+    break
+    case number === 5: return "Five"
+    break
+    case number === 6: return "Six"
+    break
+    case number === 7: return "Seven"
+    break
+    case number === 8: return "Eight"
+    break
+    case number === 9: return "Nine"
+    break
+   }
+ }
