34d4786f84b81d5c2afc0a66a124da096e6824bc
Sean Mercer <seansmercer@gmail.com>
2019-10-02 12:02:27 -0400

    Describe age

--- solutions.js
+++ solutions.js
@@ -157,3 +157,8 @@ function elevator(left, right, call){
     return "right"
   }
 }
+
+function describeAge(age) {
+  let s="You're a(n) "
+  return age<13?`${s}kid`:age<18?`${s}teenager`:age<65?`${s}adult`:`${s}elderly`
+}
