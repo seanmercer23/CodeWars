82951d918e5484dd5a6dd64f9f69c9fbc2eca26d
Sean Mercer <seansmercer@gmail.com>
2019-10-04 12:08:38 -0400

    Grade the exam

--- solutions.js
+++ solutions.js
@@ -301,3 +301,18 @@ function warnTheSheep(queue) {
 function boolToWord( bool ){
   return bool ? "Yes" : "No"
 }
+
+// Check the exam
+function checkExam(array1, array2) {
+  let grade = 0
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      grade += 4 
+    } else if (array2[i] === "") {
+      grade + 0
+    } else if (array1[i] !== array2[i]) {
+      grade--
+    }
+  }
+  return grade >= 0 ? grade : 0
+ }
