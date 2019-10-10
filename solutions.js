b888ae891f7469c32e4fdf04c3ad95c5cb36a98a
Sean Mercer <seansmercer@gmail.com>
2019-10-10 15:05:37 -0400

    Gradebook

--- solutions.js
+++ solutions.js
@@ -432,3 +432,12 @@ function checkForFactor (base, factor) {
 function greet (name, owner) {
   return name === owner ? 'Hello boss' : 'Hello guest'
 }
+
+// Grade book
+function getGrade (s1, s2, s3) {
+  const avg = (s1 + s2 + s3) / 3
+  return avg >= 90 ? 'A' : 
+         avg >= 80 ? 'B' :
+         avg >= 70 ? 'C' :
+         avg >= 60 ? 'D' : 'F'
+}
