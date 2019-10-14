f9a5b47ee11a9f8ebeb076909dfdb76730db8dea
Sean Mercer <seansmercer@gmail.com>
2019-10-14 13:51:55 -0400

    Basic math

--- solutions.js
+++ solutions.js
@@ -451,3 +451,15 @@ function squareSum(numbers){
 function litres(time) {
   return Math.floor(time / 2);
 }
+
+// Basic Math (add or subtract)
+const calculate = str =>
+  String(
+    str
+      .split('plus')
+      .join(' ')
+      .split('minus')
+      .join(' -')
+      .split(' ')
+      .reduce((total, num) => total + Number(num), 0)
+  )
