56370f5056c4b92c18bfec4d9ed7ccde6e4ebead
Sean Mercer <seansmercer@gmail.com>
2019-10-18 16:30:53 -0400

    Alternating case

--- solutions.js
+++ solutions.js
@@ -540,3 +540,18 @@ const calculate = str =>
       return nameList
     }
   }
+
+  // Alternating Case
+  String.prototype.toAlternatingCase = function () {
+    let alternatedCase = "";
+    
+    for (i = 0; i < this.length; i++) {
+      if (this[i] === this[i].toLowerCase()) {
+        alternatedCase += this[i].toUpperCase();
+      } else {
+        alternatedCase += this[i].toLowerCase();
+      }
+    }
+    
+    return alternatedCase;
+  }
